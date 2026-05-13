a21bf521a9c50960d7b23c5fc546287d1da87894
yugashreshtha <77113200+yugashreshtha@users.noreply.github.com>
2026-05-13 18:27:08 +0530

    Update thankyou.js

--- thankyou.js
+++ thankyou.js
@@ -10,26 +10,75 @@ const invoiceTotal =
 const invoiceDate =
   document.getElementById('invoice-date');
 
+const orderId =
+  document.getElementById('order-id');
+
+/* RANDOM ORDER ID */
+
+const randomOrderId =
+  Math.floor(
+    100000 + Math.random() * 900000
+  );
+
+orderId.innerText =
+  `#MB${randomOrderId}`;
+
+/* DATE */
+
 invoiceDate.innerText =
   new Date().toLocaleString();
 
+/* FOOD IMAGES */
+
+const foodImages = {
+
+  "Smoky Burger":
+  "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?q=80&w=1998&auto=format&fit=crop",
+
+  "Fire Pizza":
+  "https://images.unsplash.com/photo-1513104890138-7c749659a591?q=80&w=2070&auto=format&fit=crop",
+
+  "Creamy Pasta":
+  "https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?q=80&w=1974&auto=format&fit=crop"
+
+};
+
 let total = 0;
+
+/* SHOW ITEMS */
 
 orders.forEach((item)=>{
 
   total += item.price;
 
   invoiceItems.innerHTML += `
-  
+
     <div class="invoice-item">
 
-      <span>
-        ${item.name}
-      </span>
+      <div class="invoice-item-left">
 
-      <span>
+        <img
+          src="${foodImages[item.name]}"
+          alt=""
+        />
+
+        <div>
+
+          <h3>
+            ${item.name}
+          </h3>
+
+          <p>
+            Premium handcrafted dish
+          </p>
+
+        </div>
+
+      </div>
+
+      <div class="invoice-price">
         ₹${item.price}
-      </span>
+      </div>
 
     </div>
 
@@ -37,10 +86,18 @@ orders.forEach((item)=>{
 
 });
 
+/* TOTAL */
+
 invoiceTotal.innerText = total;
+
+/* DOWNLOAD */
 
 function downloadInvoice(){
 
   window.print();
 
 }
+
+/* CLEAR CART */
+
+localStorage.removeItem('cart');
